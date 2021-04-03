898b27693ad7efa47b2c5a5fc7760fa2351cbb3f
Jeff Liu <50810835+jeffliu6068@users.noreply.github.com>
2021-04-03 13:34:21 +0100

    Update GMMchi.py

--- GMMchi/GMMchi.py
+++ GMMchi/GMMchi.py
@@ -2,6 +2,7 @@ import GMMchi_func
 import dynamic_binning
 import solve_gaussians
 import probe_filter
+import find_hits
 
 def GMM_modelingt(ID, input_datanormal, log2transform=True, dynamic_binning_s=True, tune_factor=0.99, filt=None, verbosity=False, graphs=True,
                   graph_show=True, farinto=0.01, meanf=0, calc_back=False, stdf=0, chisquaremethod=True, unimodal_categories=True,
